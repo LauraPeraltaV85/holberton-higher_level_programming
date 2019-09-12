8bd1a840538bdbb47638853139a52db5456fb588
= <=>
2019-09-12 19:43:22 +0000

    [feature] prints number of args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv) - 1
+    if args == 0:
+        print("0: arguments.".format(args))
+    if args > 1:
+            print("{}: arguments:".format(args))
+            for argc in range(1, len(sys.argv)):
+                print("{}: {}".format(argc, sys.argv[argc]))
+    elif args == 1:
+            print("1 argument:")
+            print("{}: {}".format(args, sys.argv[1]))
