16b6625b81443418c52461a4517a6a4f6aa1be9e
= <=>
2019-09-12 19:49:18 +0000

    [fix] syntax

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     args = len(sys.argv) - 1
     if args == 0:
-        print("0: arguments.".format(args))
+        print("0 arguments.".format(args))
     if args > 1:
-            print("{}: arguments:".format(args))
+            print("{} arguments:".format(args))
             for argc in range(1, len(sys.argv)):
                 print("{}: {}".format(argc, sys.argv[argc]))
     elif args == 1:
