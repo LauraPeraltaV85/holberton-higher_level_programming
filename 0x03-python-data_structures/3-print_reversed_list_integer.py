313a1b2eeddd9541e14a79a20509e98236afe673
= <=>
2019-09-17 18:33:01 +0000

    [fix] condition null

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for x in reversed(my_list):
-        if len(my_list) == 0:
+        if my_list is None:
             break
         print("{:d}".format(x))
