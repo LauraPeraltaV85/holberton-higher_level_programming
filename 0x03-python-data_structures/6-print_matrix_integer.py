d302f1ffacdef5f7813f982539ec170ffa946ef8
= <=>
2019-09-17 22:16:58 +0000

    [fix] spaces at the end

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for x in range(len(matrix)):
         for e in range(len(matrix[x])):
-            print("{:d}".format(matrix[x][e]), end=' ')
+            print("{:d}".format(matrix[x][e]), end='')
+            if e < len(matrix[x]) - 1:
+                print("", end=' ')
         print()
