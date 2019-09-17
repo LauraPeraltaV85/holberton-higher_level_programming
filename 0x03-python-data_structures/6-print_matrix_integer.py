5fcb089930999ce27a2116d0401ee87655f058d7
= <=>
2019-09-17 21:31:07 +0000

    [feature] prints matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for x in range(len(matrix)):
         for e in range(len(matrix[x])):
-            print("{}".format(matrix[x][e]), end=' ')
+            print("{:d}".format(matrix[x][e]), end=' ')
         print()
