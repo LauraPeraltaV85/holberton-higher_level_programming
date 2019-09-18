18b05b0fe48f1b309e66136ddfcb41a849432809
= <=>
2019-09-18 00:56:42 +0000

    [fix] empty condition '

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     s = len(sentence)
-    if s is None:
-        return None
+    if s == 0:
+        sentence[0] = None
     else:
         e = (s, sentence[0])
         return(e)
