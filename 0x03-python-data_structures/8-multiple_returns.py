61947f535bc5afb690ba4a482bfc73c99d9a559c
= <=>
2019-09-18 00:41:46 +0000

    [fix] empty condition '

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     for x in range(len(sentence)):
         if sentence is None:
-            sentence[0] = None
+            return None
         else:
             e = tuple([len(sentence), sentence[0]])
             return(e)
