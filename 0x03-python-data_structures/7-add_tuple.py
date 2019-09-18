f7b5976c2f45a6f23bb63896ee1ec2d925ace67b
= <=>
2019-09-18 00:09:17 +0000

    [feature] add tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    x = list(tuple_a)
+    y = list(tuple_b)
+    if len(x) < 2:
+        for e in range(len(x), 2):
+            x.append(0)
+    if len(y) < 2:
+        for e in range(len(y), 2):
+            y.append(0)
+    n = [x[0] + y[0], x[1] + y[1]]
+    return (n)
