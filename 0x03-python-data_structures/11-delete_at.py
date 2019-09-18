ba20725ce90a75a972e9df53f6901452222aba5f
= <=>
2019-09-18 02:16:26 +0000

    [feature] deletes element at index

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    else:
+        new = my_list.copy()
+        del new[idx]
+        return new
