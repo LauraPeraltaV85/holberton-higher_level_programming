9410a6e5fd593faea2c6bd05130c19434f73e710
= <=>
2019-09-18 02:02:49 +0000

    [feature] checks if divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new = []
     for n in range(len(my_list)):
-        if n % 2 == 0:
+        if my_list[n] % 2 == 0:
             new.append(1)
         else:
             new.append(0)
