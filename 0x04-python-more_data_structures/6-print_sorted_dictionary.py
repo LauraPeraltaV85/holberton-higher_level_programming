81149e00b5dacb44176f31c1980ac08359eb1ddb
= <=>
2019-09-19 20:30:08 +0000

    [feature] prints sorted items in directory

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    [print("{} : {}".format(k, v)) for k, v in sorted(a_dictionary.items())]
+    for k, v in sorted(a_dictionary.items()):
+        print("{} : {}".format(k, v))
