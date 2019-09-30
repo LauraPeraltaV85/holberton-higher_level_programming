61adcdcd09169b6637df85358ead69598b86f3c9
= <=>
2019-09-30 18:21:30 +0000

    [fix] create new file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for t in range(x):
-            print("{}".format(my_list[t], end="")
+            print("{}".format(my_list[t]), end="")
         print()
         return x
     except IndexError:
