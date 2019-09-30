4c998c970e0b92819129c83827752da563a29614
= <=>
2019-09-30 18:14:42 +0000

    [fix] indentation

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[t], end="")
         print()
         return x
-    except IndexError as ie:
+    except IndexError:
         print()
         return t
