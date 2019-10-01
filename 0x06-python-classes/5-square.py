310f8c008227978a2b8da4f265f5513de77e6d41
= <=>
2019-10-01 20:31:12 +0000

    [feature] prints square of hashtags

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+class Square:
+    """defines a square area, uses property and
+       setter to set variables and prints square
+       of hashtags"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for count in range(self.size):
+                for count in range(self.size):
+                    print('#', end='')
+                print()
