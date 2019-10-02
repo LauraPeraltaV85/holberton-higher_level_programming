3dfa11912e41c453d812c18f42b68f90bd42fb52
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-10-02 03:38:39 +0000

    [feature] bytecode #5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import math
 
+
 class MagicClass:
+    """bytecode"""
+
     def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self.__radius = radius
+        else:
+            self.__radius = radius
 
     def area(self, radius=0):
         return (self.__radius**2) * math.pi
