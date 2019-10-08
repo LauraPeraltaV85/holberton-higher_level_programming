df9878a9d56469269109f28d86be5303b5561ea2
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-10-08 02:55:43 +0000

    [fix] init syntax

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Rectangle:
     """Defines a rectangle"""
-    def init(self, width=0, height=0):
+    def __init__(self, width=0, height=0):
             self.__width = width
             self.__height = height
 
