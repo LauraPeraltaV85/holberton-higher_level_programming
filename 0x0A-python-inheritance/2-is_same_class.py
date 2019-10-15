e6ece6635f247963b038b484b5ead49f6d3c8e98
= <=>
2019-10-15 20:50:54 +0000

    [feature] returns true if isntance exactly of specific class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Is object exactly an instance of specific class
+"""
+
+
+def is_same_class(obj, a_class):
+    """Function that returns True if the
+    object is exactly an instance of the
+    specified class; otherwise False.
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
