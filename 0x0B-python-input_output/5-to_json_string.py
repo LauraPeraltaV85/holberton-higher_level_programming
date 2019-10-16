4aa57f3b95f7de3a60fd313b601d122754881af1
= <=>
2019-10-16 20:13:37 +0000

    [feature] returns representation of string

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def to_json_string(my_obj):
+    """function that returns the JSON
+    representation of an object (string)
+    """
+    import json
+    string = json.dumps(my_obj)
+    return string
