58c85479a2328c8064506631849d7511cacdb095
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-10-22 22:13:39 +0000

    [fix]

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,17 +10,19 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """defines square, inherits from rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
+        """initializes attributes"""
         self.size = size
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        return self.width
+        """defines size"""
+        return self.size
 
     @size.setter
     def size(self, value):
         """assignation"""
-        self.width = value
+        self.size = value
         self.height = value
 
     def __str__(self):
