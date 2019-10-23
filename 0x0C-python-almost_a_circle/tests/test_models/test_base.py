33ed4f07d690d0b9c91a8dcf8b794609d017b80c
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-10-23 01:45:49 +0000

    [fix]

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -40,5 +40,46 @@ class test_base(unittest.TestCase):
         b5 = Base(-5)
         self.assertEqual((b5.id), (-5))
 
+    def test_empty_json(self):
+        """tests if list is empty"""
+        lis = Base.to_json_string([])
+        self.assertEqual(lis, "[]")
+
+    def test_emptydict_json(self):
+        """tests for empty dict"""
+        dic = Base.to_json_string([{}])
+        self.assertEqual(dic, "[{}]")
+
+    def test_nonefile(self):
+        """tests for none file"""
+        Base.save_to_file(None)
+        with open("Base.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
+
+    def test_from_string_set(self):
+        """tests func from_json with set"""
+        with self.assertRaises(TypeError) as e:
+            Base.from_json_string({1, 2})
+        self.assertEqual("the JSON object must be str, not 'set'", str(e.exception))
+
+    def test_empty_list_to_file(self):
+        """Test empty list for save_to_file method"""
+        Base.save_to_file([])
+        with open("Base.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
+
+    def test_json_list_dicts(self):
+        """Test for multiple dicts in to_json_string."""
+        lis = Base.to_json_string([{"a": 1}, {"b": 2}])
+        self.assertEqual(type(lis), str)
+
+
+    def test_32_from_json_string_list(self):
+        """Test for from_json_method with list."""
+        with self.assertRaises(TypeError) as e:
+            Base.from_json_string([1, 2, 3])
+        self.assertEqual("the JSON object must be str, not 'list'",
+                         str(e.exception))
+
 if __name__ == '__main__':
     unittest.main()
