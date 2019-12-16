30c384b8a0342c906b1588028d5ed8f202e418dc
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-12-16 21:46:36 +0000

    [Feature] print first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhos\
+t/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_states = session.query(State).order_by(State.id).first()
+
+    print("{}: {}".format(new_states.id, new_states.name))
+    session.close()
