6b0ad102f484cd7ed964d5538311608059cb8421
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-01-07 02:50:22 +0000

    [feature] content length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a request to that URL, and displays the size
-# of the body of the response
+# sends a request, and displays the size of the body of the response
 curl -Is "$1" | grep Content-Lenght | cut -d " " -f2
