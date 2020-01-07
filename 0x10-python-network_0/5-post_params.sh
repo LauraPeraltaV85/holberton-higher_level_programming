073d84d18c53dcdb59eead23adcf54fd2a1d3bb7
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-01-07 04:43:46 +0000

    [feature] post method

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL, and displays the body of the response
-curl -sL -X POST -d "email=hr@holbertonschool.com&subject=I will alwaysbe here for PLD" $1
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will alwaysbe here for PLD" $1
