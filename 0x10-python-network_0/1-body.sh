1945abeb8d268ab94164da635bf9b1071517db4d
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-01-07 02:42:31 +0000

    [feature] body response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a GET request to the URL, and 
-# displays the body of the response
+# sends a GET request to the URL, and displays the body of the response
 curl -Ls -X -G "$1"
