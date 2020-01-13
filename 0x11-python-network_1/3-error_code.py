e369eb5ecbf3345dbb8d8ad15971b8e6bab5bfcd
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-01-13 23:39:59 +0000

    [feature] manage error http

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""takes URL sends request to URL displays
+body of response (decoded in utf-8)"""
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import urllib.error
+    try:
+        with urlib.request.urlopen(sys.argv[1]) as response:
+            print(response.read())
+        except urllib.error.HTTPError as e:
+            print("Error code:", e.code)
