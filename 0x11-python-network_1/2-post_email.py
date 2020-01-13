bf91a855ca1f20737e45550cf2b2d42e30f59156
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-01-13 22:23:20 +0000

    [feature] add email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" takes URL and email, sends POST request to passed URL
+with email parameter, displays body response (decoded in utf-8)"""
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+    url = argv[1]
+    values = {'email': argv[2]}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("Your email is: {}".format(argv[2]))
