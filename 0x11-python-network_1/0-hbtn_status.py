efca7f07c448efffbec6d9788ef61e6f4d289f44
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-01-13 17:26:50 +0000

    [feature] status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,8 +2,7 @@
 """fetch https://intranet.hbtn.io/status"""
 if __name__ == "__main__":
     import urllib.request
-    requ = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(requ) as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t - type: {}".format(type(html)))
