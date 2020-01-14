0862772d72283bb195e3663fcd7daeec5165d7b5
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-01-14 02:35:10 +0000

    [feature] api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = "http://0.0.0.0.5000/search_user"
+    if sys.argv[1]:
+        q = sys.argv[1]
+    else:
+        q = ""
+    r = requests.post(url, q=q)
+    try:
+        req = r.json()
+        if not req:
+            print("No result")
+        else:
+            print("[{}] {}".format(req.get("id"), req.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
