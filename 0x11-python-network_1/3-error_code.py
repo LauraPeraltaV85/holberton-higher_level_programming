6b9a30285ae8764eb7f767d47f44f4f916531056
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-01-14 00:09:44 +0000

    [feature] manage error http

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     import sys
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
-            print(response.read())
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error code:", e.code)
