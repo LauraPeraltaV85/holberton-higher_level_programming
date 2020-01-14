d1ef34bb486ab848f73d8551fabd80cae0ae0a04
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-01-14 03:09:52 +0000

    [sajnfjiasnf]

---
+++ 0x11-python-network_1/103-search_twitter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""takes your Github credentials (username and password)
+and uses the Github API to display your id"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    import base64
