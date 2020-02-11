d611bf6de81f2c468af4ba6ad83b6cff9493bf0c
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-02-11 17:49:20 +0000

    [fix] error to log

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const args = process.argv[2];
 fs.readFile(args, 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
     return;
   }
   console.log(data);
